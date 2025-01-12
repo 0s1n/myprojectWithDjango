59b8c1ab414048986d7df0f9f9297f2f43a4de48
0s1n <jpablo_166@hotmail.com>
2025-01-12 16:53:32 -0500

    feat added post:slug

--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,9 @@ def posts_list(request):
     return render(request, 'posts/posts_list.html', {
         'posts': posts
     })
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {
+        'post': post
+    })
