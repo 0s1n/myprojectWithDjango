12291b908d0ff377c3cfd549f2d75a074a123d73
0s1n <jpablo_166@hotmail.com>
2025-01-12 19:41:43 -0500

    feat: added new_post route & html

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def posts_list(request):
@@ -13,3 +14,7 @@ def post_page(request, slug):
     return render(request, 'posts/post_page.html', {
         'post': post
     })
+
+@login_required(login_url='/users/login/')
+def new_post(request):
+    return render(request, 'posts/new_post.html')
